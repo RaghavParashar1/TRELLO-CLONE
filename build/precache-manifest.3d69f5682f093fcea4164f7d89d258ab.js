5aaf077f0e4ce35276280dbcf1f8a8fb77b66cf0
RaghavParashar1 <rparasharr@gmail.com>
2021-06-28 17:21:26 +0530

    added new build
    Signed-off-by: RaghavParashar1 <rparasharr@gmail.com>

---
+++ build/precache-manifest.3d69f5682f093fcea4164f7d89d258ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9710f7e61f132cde32a8",
+    "url": "/trello-clone/static/js/runtime~main.9710f7e6.js"
+  },
+  {
+    "revision": "575a427d94767d62614a",
+    "url": "/trello-clone/static/js/main.575a427d.chunk.js"
+  },
+  {
+    "revision": "0acc705910af295ca4f3",
+    "url": "/trello-clone/static/js/2.0acc7059.chunk.js"
+  },
+  {
+    "revision": "575a427d94767d62614a",
+    "url": "/trello-clone/static/css/main.5189f011.chunk.css"
+  },
+  {
+    "revision": "f73571b9ce513d487244048a22a91f26",
+    "url": "/trello-clone/index.html"
+  }
+];
